668e6eef86fa3558e2f5b41810cd75805d687e76
Paul0205333 <pauledwardtablizo30@gmail.com>
2025-06-04 12:51:20 +0800

    Update AuthenticationSystem.java

--- HotelReservationSystem/src/system/AuthenticationSystem.java
+++ HotelReservationSystem/src/system/AuthenticationSystem.java
@@ -1,5 +1,21 @@
 package system;
 
+import java.util.*;
+
 public class AuthenticationSystem {
+	
+	private HashMap<String, User> users = new HashMap<>();
+	private User currentUser;
+	
+	public boolean registerUser(String name, String email, String password, String phoneNumber) {
+	
+		
+		
+		
+		
+		
+ 		
+		return true;
+	}
 
 }
