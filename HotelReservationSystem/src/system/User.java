c57b2906e2bdf0217199f29a67cb5f9ef090d609
Paul0205333 <pauledwardtablizo30@gmail.com>
2025-06-04 12:51:52 +0800

    Update User.java

--- HotelReservationSystem/src/system/User.java
+++ HotelReservationSystem/src/system/User.java
@@ -1,5 +1,37 @@
 package system;
 
 public class User {
-
+	private String userID;
+	private String name;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	
+	User(String userID, String name,String email, String password, String phoneNumber){
+		this.userID = userID;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getUser() {
+		return userID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 }
