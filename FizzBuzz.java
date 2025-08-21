88a082b409d819cf32afe8175bc5387f9f517176
Paul0205333 <pauledwardtablizo30@gmail.com>
2025-08-21 11:02:53 +0800

    FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -16,12 +16,13 @@ public class FizzBuzz {
 		if(number%5 == 0 && number%3 == 0) {
 			System.out.println("FizzBuzz");
 		} else if (number%3 == 0) {
-			System.out.println("Buzz");
-		} else if (number%5 == 0) {
 			System.out.println("Fizz");
+		} else if (number%5 == 0) {
+			System.out.println("Buzz");
 		} else {
 			System.out.println(number);
 		}
 		
 	}
 }
+
