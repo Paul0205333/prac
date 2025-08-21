6e8f029932b01382c8c4f4e807b42b6d7bf5b0c0
Paul0205333 <pauledwardtablizo30@gmail.com>
2025-08-21 11:01:12 +0800

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,27 @@
+package FizzBuzz;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+	
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Number: ");
+		int number = scanner.nextInt();
+		
+		scanner.close();
+		
+		if(number%5 == 0 && number%3 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (number%3 == 0) {
+			System.out.println("Buzz");
+		} else if (number%5 == 0) {
+			System.out.println("Fizz");
+		} else {
+			System.out.println(number);
+		}
+		
+	}
+}
