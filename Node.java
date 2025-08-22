9dd0cd8262f054359c476a68b83cea9d8c5caf61
Paul0205333 <pauledwardtablizo30@gmail.com>
2025-08-22 20:03:34 +0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -4,8 +4,10 @@ public class Node {
 	String data;
 	Node next; 
 	
+	// Constructor that assigns values to each Node object
 	 public Node(String data) {
 	       this.data = data;
 	       this.next = null; 
 	   }
 }
+
