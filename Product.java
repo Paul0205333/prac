0713022cb1408e539fa0826e3ec0084571b6f76b
Paul0205333 <pauledwardtablizo30@gmail.com>
2025-10-15 11:20:34 +0800

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,14 @@
+package storeManagementSystem;
+
+public class Product {
+
+	String name;
+	int stock;
+	double price;
+	
+	Product(String name, double price, int stock){
+		this.name = name;
+		this.stock = stock;
+		this.price = price;
+	}
+}
